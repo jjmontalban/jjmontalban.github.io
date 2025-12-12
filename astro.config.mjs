857cff1e69bdb43afb26880482e6d07bb3b0181a
Joaquin Jurado <joaquin.jurado@experienceis.com>
2025-12-12 16:04:24 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jjmontalban.github.io',
+  base: '/portafolio/',
+});
